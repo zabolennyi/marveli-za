6c6e6be77cc83c086bb024d390096bbc8053c51c
Serhii Zabolennyi <zabolennyy.sergey@gmail.com>
2019-04-23 09:16:19 +0200

    _sec

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 # marveli-za
 
 > A Vue.js project
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .wrapper
+    #app
+      h1 Hi
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus">
+#app
+  padding 30px
+  background-color #333;
+  color #ffffff;
+
+</style>
