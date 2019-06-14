8fdbb66806d4ff55fb4402c4a11118ec7032d78c
Serhii Zabolennyi <zabolennyy.sergey@gmail.com>
2019-06-14 12:29:46 +0200

    _sec

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,51 @@
     section
       .container
         h1.ui-title-1 Home
+        input(
+          type="text"
+          placeholder="What we will watch?"
+          v-model="taskTitle"
+          @keyup.enter="newTask"
+        )
+        textarea(
+          type="text"
+          placeholder="Description"
+          v-model="taskDescription"
+          @keyup.enter="newTask"
+        )
+        .option-list
+          input.what-watch--radio(
+            type="radio"
+            id="radioFilm"
+            value="Film"
+            v-model="whatWatch"
+          )
+          label(
+            for="radioFilm"
+          ) Film
+          input.what-watch--radio(
+            type="radio"
+            id="radioSerial"
+            value="Serial"
+            v-model="whatWatch"
+          )
+          label(
+            for="radioSerial"
+          ) Serial
+        .total-time
+          .total-time__film(
+            v-if="whatWatch === 'Film'"
+          )
+            span Total Film Time
+          .total-time__film(
+            v-if="whatWatch === 'Serial'"
+          )
+            span Total Serial Time
+        .tag-list
+        .ui-tag__wrapper
+          .ui-tag
+            span.tag-title Phase 1
+            span.button-close
     section
       .container
         .task-list
@@ -14,23 +59,34 @@
             .ui-card.ui-card--shadow
               .task-item__info
                 .task-item__main-info
-                  span.ui-label.ui-label--light {{ task.whatWhatch }}
+                  span.ui-label.ui-label--light {{ task.whatWatch }}
                   span Total Time:
                 span.button-close
-              .task-item__content
+              .task-item__header
+                .ui-checkbox-wrapper
+                  input.ui-checkbox(
+                    type='checkbox'
+                    v-model="task.completed"
+                    )
                 span.ui-title-3 {{ task.title }}
+              .task-item__body
+              p.ui-text-regular {{ task.description }}
 
 </template>
 <script>
 export default {
   data () {
     return {
+      taskTitle: '',
+      taskId: 3,
+      taskDescription: '',
+      whatWatch: 'Film',
       tasks: [
         {
           'id': 1,
           'title': 'Iron Man',
           'description': 'aaaaa',
-          'whatWhatch': 'Film',
+          'whatWatch': 'Film',
           'completed': false,
           'editing': false
         },
@@ -38,17 +94,46 @@ export default {
           'id': 2,
           'title': 'Doctor Strange',
           'description': 'bbbbb',
-          'whatWhatch': 'Serial',
+          'whatWatch': 'Serial',
           'completed': false,
           'editing': false
         }
       ]
+    }
+  },
+  methods: {
+    newTask () {
+      if (this.taskTitle === '') {
+        return
+      }
+      this.tasks.push({
+        id: this.taskId,
+        title: this.taskTitle,
+        description: this.taskDescription,
+        whatWatch: this.whatWatch,
+        completed: false,
+        editing: false
+      })
+
+      //  reset
+      this.taskId += 1
+      this.taskTitle = ''
+      this.taskDescription = ''
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+.option-list
+  display flex
+  .what-watch--radio
+    margin-right 12px
+  .label
+    margin-right 20px
+    &:last-child
+      margin-right  0
+
 .task-item
   margin-bottom 20px
   &:last-child
@@ -65,4 +150,13 @@ export default {
   .button-close
     width 20px
     height @width
+
+.task-item__header
+  display flex
+  align-items center
+  margin-bottom 18px
+  .ui-checkbox-wrapper
+    margin-right 8px
+  .ui-title-3
+    margin-bottom 0
 </style>
