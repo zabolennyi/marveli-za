eb976956bee396707be31261deb92165e1cb1721
Serhii Zabolennyi <zabolennyy.sergey@gmail.com>
2019-07-09 14:47:22 +0200

    addedf vuelidate

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -2,16 +2,44 @@
      .content-wrapper
         section
             .container
-            h1.ui-title-1 Registration
+              .auth
+                .auth__banner
+                  h1.ui-title-2 banner
+                .auth__form
+                  span.ui-title-2 Registration
+                    form
+                      input(
+                        type="email"
+                        placeholder="email"
+                        v-model="email"
+                      )
+                      input(
+                        type="password"
+                        placeholder="password"
+                        v-model="password"
+                      )
+                      input(
+                        type="password"
+                        placeholder="Repear Pass"
+                        v-model="repeatPassword"
+                      )
 </template>
 <script>
 export default {
   data () {
-    return {}
+    return {
+      email: '',
+      password: '',
+      repeatPassword: ''
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.auth
+  display flex
+.auth__banner
+.auth__form
+  width 50%
 </style>
